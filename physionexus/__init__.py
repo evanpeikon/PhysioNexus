754f16f464330a6486c37d704a6e3cb6d307ac0d
Evan Peikon <154470963+evanpeikon@users.noreply.github.com>
2025-03-04 13:42:19 -0500

    Update __init__.py

--- physionexus/__init__.py
+++ physionexus/__init__.py
@@ -1,5 +1,2 @@
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'code'))
-from PhysioNexus import PhysioNexus  
+from .PhysioNexus import PhysioNexus
 __version__ = "0.1.0"
